1cdd1013d5edf4da2a00551fbbae8d07628a9ab0
samarth1998 <s2aggarw@ucsd.edu>
2018-11-01 10:53:41 -0700

    changes

---
+++ js/home.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  initializePage();
+})
+  
+  function initializePage() {
+    $("#avatar1").click(function(changeText);
+    function changeText(event) {
+      $('this').text("Hi connect you with a remote doctor who can diagnose your health issues and provide recomendations.");
+      }
+  }
